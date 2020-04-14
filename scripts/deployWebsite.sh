d34097ad7cbb3eb7fc57c11b7232fe2ff269a754
Angelos Angelopoulos <aangelos28@gmail.com>
2020-04-14 15:34:22 -0400

    Update technical risk mitigation and change deployment script to use rsync

--- scripts/deployWebsite.sh
+++ scripts/deployWebsite.sh
@@ -9,36 +9,16 @@ REMOTE_SERVER_USERNAME="410yello"
 REMOTE_SERVER_NAME="atria.cs.odu.edu"
 REMOTE_SERVER_CONNECTION_STRING=${REMOTE_SERVER_USERNAME}@${REMOTE_SERVER_NAME}
 REMOTE_SERVER_DEPLOYMENT_PATH="secure_html"
-WEBSITE_ARCHIVE_NAME="website.tar.gz"
 
-echo "Creating tar archive of exported website..."
-cd dist/ || (echo "Please export the website first" && return)
-tar -cvzf $WEBSITE_ARCHIVE_NAME *
+echo "Deploying website to $REMOTE_SERVER_NAME..."
+rsync -rzP dist/ --exclude={'scripts/compileStyles.sh', 'scripts/deployWebsite.sh', 'scripts/exportWebsite.sh'} \
+ ${REMOTE_SERVER_CONNECTION_STRING}:${REMOTE_SERVER_DEPLOYMENT_PATH}
 
-echo -e "\nPurging old website content from $REMOTE_SERVER_NAME...\n"
-ssh -T $REMOTE_SERVER_CONNECTION_STRING << EOF
-  cd $REMOTE_SERVER_DEPLOYMENT_PATH || (echo "Could not find website directory on $REMOTE_SERVER_NAME" && return)
-  rm -rf *
-  exit
-EOF
-
-echo -e "\nUploading website archive to $REMOTE_SERVER_NAME...\n"
-sftp $REMOTE_SERVER_CONNECTION_STRING << EOF
-  cd $REMOTE_SERVER_DEPLOYMENT_PATH || return
-  put $WEBSITE_ARCHIVE_NAME
-  exit
-EOF
-
-echo -e "\nExtracting website archive and setting permissions...\n"
+echo -e "\nSetting permissions...\n"
 ssh -T $REMOTE_SERVER_CONNECTION_STRING << EOF
   cd $REMOTE_SERVER_DEPLOYMENT_PATH || return
-  tar -xvf $WEBSITE_ARCHIVE_NAME
-  rm $WEBSITE_ARCHIVE_NAME
   scripts/setPermissions.sh
   exit
 EOF
-
-echo -e "\nDeleting local website archive...\n"
-rm $WEBSITE_ARCHIVE_NAME
 
 echo -e "\nFinished deploying website to $REMOTE_SERVER_NAME.\n"
